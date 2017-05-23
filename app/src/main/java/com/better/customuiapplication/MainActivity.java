bd6ceb12cb971284c4819353976bdcb7aef1c27f
李庆年 <1713923044@qq.com>
2017-05-23 15:05:36 +0800

    1、一般情况，点击按钮按钮颜色、边框颜色都会改变，所以做了一些修改

--- app/src/main/java/com/better/customuiapplication/MainActivity.java
+++ app/src/main/java/com/better/customuiapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.better.customuiapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
